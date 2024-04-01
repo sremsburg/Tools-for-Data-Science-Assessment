832e5eef243c2421dd25b0a41c83a56585884881
sremsburg <165612053+sremsburg@users.noreply.github.com>
2024-04-01 12:45:29 -0400

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**
+# - Create and rename a Jupyter Notebook
+# - Create a markdown cell
+# - Insert a markdown cell
+# - Creating and executing a code cell
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Seaborn
+# 5. SciKit Learn
+
+# |Data Science Tools|
+# |------------------|
+# |Apache Airflow|
+# |Hadoop File System|
+# |TensorFlow|
+
+# ### Below are a few examples of evaluating arithmetic expression in Python
+
+# In[4]:
+
+
+#this is a simple arithmetic expression to multiple then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# ## Author
+# Sarah Remsburg
+
+# In[ ]:
+
+
+
+
